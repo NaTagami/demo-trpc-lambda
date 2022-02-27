ec3cd033e5a005312c17f113f9841961d30105aa
Naoki Tagami <n.tagami@kakehashi.life>
2022-02-27 15:28:16 +0900

    add sample-app.ts

---
+++ cdk/bin/sample-app.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import * as cdk from "@aws-cdk/core";
+import { CdkApiStack } from "../lib/apiGatewayStack";
+import { CdkLambdaStack } from "../lib/lambdaStack";
+
+const app = new cdk.App();
+new CdkLambdaStack(app, "LambdaStack");
+new CdkApiStack(app, "ApiGatewayStack");
